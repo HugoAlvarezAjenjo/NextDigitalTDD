902bf55757cbc0e65fdad3ef903fca1eb5667e9a
Hugo Alvarez Ajenjo <hugo.alvarezajenjo@gmail.com>
2025-01-30 12:40:01 +0100

    initial commit

---
+++ src/main/java/es/hugoalvarezajenjo/windows/TennisGame.java
@@ -0,0 +1,32 @@
+package es.hugoalvarezajenjo.windows;
+
+import java.util.Map;
+
+public class TennisGame {
+
+    private int player1Score = 0;
+    private int player2Score = 0;
+    Map<Integer, String> scoring = Map.of(
+            0, "love",
+            1, "fifteen",
+            2, "thirty",
+            3, "forty"
+    );
+
+    public String getScore() {
+        if (player1Score == 3 && player2Score == 3) {
+            return "deuce";
+        }
+        final String p1score = scoring.get(player1Score);
+        final String p2score = scoring.get(player2Score);
+        return p1score + "-" + p2score;
+    }
+
+    public void playerScore1() {
+        this.player1Score = player1Score + 1;
+    }
+
+    public void playerScore2() {
+        player2Score++;
+    }
+}
---
+++ src/test/java/es/hugoalvarezajenjo/windows/AppTest.java
@@ -0,0 +1,52 @@
+package es.hugoalvarezajenjo.windows;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class AppTest {
+    @Test
+    void shouldReturnLoveOnStart() {
+        TennisGame game = new TennisGame();
+        String score = game.getScore();
+        assertEquals("love-love", score);
+    }
+
+    @Test
+    void shouldReturnFifteenLoveOnPlayerOneScoreOne() {
+        TennisGame game = new TennisGame();
+        game.playerScore1();
+        String score = game.getScore();
+        assertEquals("fifteen-love", score);
+    }
+    @Test
+    void shouldReturnFifteenLoveOnPlayerOneScoreTwo() {
+        TennisGame game = new TennisGame();
+        game.playerScore1();
+        game.playerScore1();
+        String score = game.getScore();
+        assertEquals("thirty-love", score);
+    }
+
+    @Test
+    void shouldReturnFortyLoveOnPlayerOneScoreThree() {
+        TennisGame game = new TennisGame();
+        game.playerScore1();
+        game.playerScore1();
+        game.playerScore1();
+        String score = game.getScore();
+        assertEquals("forty-love", score);
+    }
+
+    @Test
+    void shouldReturnDeuceIfBothPlayersScoreThree() {
+        TennisGame game = new TennisGame();
+        game.playerScore1();
+        game.playerScore1();
+        game.playerScore1();
+        game.playerScore2();
+        game.playerScore2();
+        game.playerScore2();
+        String score = game.getScore();
+        assertEquals("deuce", score);
+    }
+}
